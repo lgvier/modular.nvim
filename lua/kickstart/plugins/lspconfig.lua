30a0c7fd78ed01435dd15862760bfd86e2124829
brazooka <lgvier@gmail.com>
2025-09-14 03:28:09 -0400

    tweaks

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -211,6 +211,7 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
+        basedpyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
