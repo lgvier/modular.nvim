a84ec099b305fc3d4121be30778a7490096eddea
brazooka <lgvier@gmail.com>
2025-09-28 20:19:17 -0400

    tweaks

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,29 +13,30 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-    event_handlers = {
-      {
-        event = 'file_open_requested',
-        handler = function()
-          -- auto close
-          -- vim.cmd("Neotree close")
-          -- OR
-          require('neo-tree.command').execute { action = 'close' }
-        end,
-      },
-    },
-  },
   config = function()
     require('neo-tree').setup {
       close_if_last_window = true, -- Set to true to close Neo-tree if it's the last window
+      filesystem = {
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+          },
+        },
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true, -- You can adjust this as needed
+        },
+      },
+      event_handlers = {
+        -- {
+        --   event = 'file_open_requested',
+        --   handler = function()
+        --     -- auto close
+        --     require('neo-tree.command').execute { action = 'close' }
+        --   end,
+        -- },
+      },
       -- Other Neo-tree configuration options...
     }
     vim.api.nvim_create_autocmd('VimEnter', {
